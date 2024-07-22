236f29a4552eb36a6bc8a0b00c131b491bdc67f9
Force Fidele KIEN <forcefidele@gmail.com>
2024-07-22 23:06:35 +0200

    feat: add script to automate committing updated en-kh.angkor.sqlite

---
+++ update-en-kh-database.ps1
@@ -0,0 +1,4 @@
+Copy-Item -Path D:\Dropbox\Projects\Dictionaries\Databases\en-kh.angkor.sqlite . -Recurse -Force
+git add en-kh.angkor.sqlite
+git commit -m "feat: update en-kh.angkor.sqlite"
+git push -u origin main
