cef08f0aae942f39663223c2a9fe341b8637e6c2
Force Fidele KIEN <forcefidele@gmail.com>
2025-01-23 21:19:38 +0100

    feat: update update-en-kh-database scripts

--- update-en-kh-database.ps1
+++ update-en-kh-database.ps1
@@ -1,6 +1,6 @@
 git pull
 
-Copy-Item -Path D:\Dropbox\Projects\Dictionaries\Databases\en-kh.angkor.sqlite . -Recurse -Force
+# Copy-Item -Path D:\Dropbox\Projects\Dictionaries\Databases\en-kh.angkor.sqlite . -Recurse -Force
 
 git add en-kh.angkor.sqlite
 git commit -m "feat: update en-kh.angkor.sqlite"
